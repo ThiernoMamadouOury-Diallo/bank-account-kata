82ff8ab3c1d210d7a14cc74d6b1b62cb8d4e26ea
Thierno <mamyourydiallo@gmail.com>
2019-07-09 20:30:03 +0200

    data for operations array

---
+++ src/main/java/com/socgen/itec/bank/account/kata/constants/OperationsData.java
@@ -0,0 +1,61 @@
+package com.socgen.itec.bank.account.kata.constants;
+
+import com.socgen.itec.bank.account.kata.model.Operation;
+import com.socgen.itec.bank.account.kata.model.Operations;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public enum OperationsData {
+    OPERATIONS_DATA1("Retrait1", "09/08/2019", "20", "50"),
+    OPERATIONS_DATA2("Depot2","10/08/2019", "20", "70"),
+    OPERATIONS_DATA3("Virement3", "11/08/2019", "20", "50");
+
+    private final String operation;
+    private final String date;
+    private final String amount;
+    private final String balance;
+
+    public String getOperation() {
+        return operation;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public String getAmount() {
+        return amount;
+    }
+
+    public String getBalance() {
+        return balance;
+    }
+
+    OperationsData(final String operation, final String date, final String amount, final String balance){
+        this.operation = operation;
+        this.date = date;
+        this.amount = amount;
+        this.balance = balance;
+
+    }
+    public static Operations getOperations(){
+        Operations operations = new Operations();
+        List<Operation> operationList = new ArrayList<>();
+        Arrays.asList(OperationsData.values()).forEach(e->operationList.add(initOperation(e.operation, e.date, e.amount, e.balance)));
+        operations.setOperations(operationList);
+        operations.setUserId("User1");
+        return operations;
+    }
+    public static Operation initOperation(String oper, String date, String amount, String balance){
+        Operation operation = new Operation();
+        operation.setOperation(oper);
+        operation.setDate(date);
+        operation.setAmount(amount);
+        operation.setBalance(balance);
+        return operation;
+
+    }
+
+}
--- src/main/java/com/socgen/itec/bank/account/kata/model/Operation.java
+++ src/main/java/com/socgen/itec/bank/account/kata/model/Operation.java
@@ -3,9 +3,6 @@ package com.socgen.itec.bank.account.kata.model;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.*;
 
-import java.math.BigDecimal;
-import java.util.Date;
-
 @Getter
 @Setter
 @NoArgsConstructor
@@ -17,7 +14,7 @@ public class Operation {
     private static final long serialVersionUID = 1L;
 
     private String operation;
-    private Date date;
-    private BigDecimal amount;
-    private BigDecimal balance;
+    private String date;
+    private String amount;
+    private String balance;
 }
