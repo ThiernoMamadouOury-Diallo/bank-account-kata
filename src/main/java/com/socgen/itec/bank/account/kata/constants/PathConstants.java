f5022867d9ad28a019e001a1838d6c85c7eab1ce
Thierno <mamyourydiallo@gmail.com>
2019-07-10 02:46:10 +0200

    userId added URI

--- src/main/java/com/socgen/itec/bank/account/kata/constants/PathConstants.java
+++ src/main/java/com/socgen/itec/bank/account/kata/constants/PathConstants.java
@@ -4,7 +4,7 @@ package com.socgen.itec.bank.account.kata.constants;
 public class PathConstants {
     /* operations paths */
     public static final String REST_API_PATH ="/api";
-    public static final String REST_API_DEPOSIT_PATH ="/deposit";
+    public static final String REST_API_DEPOSIT_PATH ="{idUser}/deposit/{amount}";
     public static final String REST_API_WITHDRAWAL_PATH ="withdrawal";
     public static final String REST_API_OPERATIONS_PATH ="{idUser}/operations";
 }
